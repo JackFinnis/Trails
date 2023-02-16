31a88635917d869c4a557e14f1ac935d05cd1802
Jack Finnis <jack.finnis@icloud.com>
2023-02-16 16:30:43 +0000

    Initial Commit

---
+++ Trails/TrailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailsApp.swift
+//  Trails
+//
+//  Created by Jack Finnis on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
