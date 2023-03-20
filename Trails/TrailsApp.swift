c7c5d32f989eb435ba42cd0f5f7ab892fc42d255
Jack Finnis <jack.finnis@icloud.com>
2023-03-20 00:37:31 +0000

    New app id

--- Trails/TrailsApp.swift
+++ Trails/TrailsApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 let NAME = "National Trails"
 let SIZE = 48.0
 let EMAIL = "jack.finnis@icloud.com"
-let APP_URL = URL(string: "https://apps.apple.com/app/id6443834631")!
+let APP_URL = URL(string: "https://apps.apple.com/app/id6446465966")!
 
 @main
 struct TrailsApp: App {
